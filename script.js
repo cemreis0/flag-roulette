020636f1a24da08b70506e1c1c2e1547fd2fb148
cemreis0 <cemmreis@gmail.com>
2023-06-05 13:58:12 +0300

    added randomizing 3 columns

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const flag = document.querySelector(".flag")
 const generateFlag = document.querySelector(".generateFlag")
+const downloadFlag = document.querySelector(".downloadFlag")
 
-generateFlagFunc(flag)
-
-generateFlag.addEventListener("click", function () {
+generateFlag.addEventListener("click", () => {
   generateFlagFunc(flag)
 })
 
-function generateFlagFunc(flag) {
+downloadFlag.addEventListener("click", () => {
+  downloadFlagFunc(flag)
+})
+
+const generateRandomColorFunc = () => {
   const red = Math.floor(Math.random() * 256)
   const green = Math.floor(Math.random() * 256)
   const blue = Math.floor(Math.random() * 256)
-  flag.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+  const randomColor = `rgb(${red}, ${green}, ${blue})`
+  return randomColor
+}
+
+const generateFlagFunc = (flag) => {
+  const canvasContext= flag.getContext("2d")
+  canvasContext.fillStyle = generateRandomColorFunc()
+  canvasContext.fillRect(0, 0, flag.width/3, flag.height)
+  canvasContext.fillStyle = generateRandomColorFunc()
+  canvasContext.fillRect(flag.width/3, 0, flag.width/3, flag.height)
+  canvasContext.fillStyle = generateRandomColorFunc()
+  canvasContext.fillRect(flag.width/3*2, 0, flag.width/3, flag.height)
+}
+
+const downloadFlagFunc = (flag) => {
+  const canvas = document.createElement('canvas')
+  const width = 1000;
+  const height = 600;
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext('2d')
+  context.drawImage(flag, 0, 0, width, height)
+  const dataURL = canvas.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream')
+  const anchor = document.createElement('a')
+  const filename = 'flag.jpeg'
+  anchor.setAttribute('href', dataURL)
+  anchor.setAttribute('download', filename)
+  anchor.click()
 }
